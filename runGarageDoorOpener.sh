380a51be49bdd6ff6164c0153414aee8d5bd8d84
TJ Mercier <thomas.mercier.jr@gmail.com>
2016-02-06 17:05:49 -0500

    Exec instead of fork in run script Fixes #1

--- runGarageDoorOpener.sh
+++ runGarageDoorOpener.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-java -Djava.security.properties=GarageDoorOpener.properties -jar build/libs/GarageDoorOpener-0.9.jar 
+BASEDIR=$(dirname $0)
+cd "$BASEDIR"
+
+exec java -Djava.security.properties=GarageDoorOpener.properties -jar "$BASEDIR"/build/libs/GarageDoorOpener-0.9.jar 
